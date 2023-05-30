5d255ad8dc24fac3656e9e6acbcf9d2fcc447952
jiamingkong <kinetical@live.com>
2023-05-30 15:19:03 +0800

    Added FAISS example to showcase retrieval_qa

--- rwkv_chains/retrieval_qa/__init__.py
+++ rwkv_chains/retrieval_qa/__init__.py
@@ -1,1 +1,3 @@
 """Chain for question-answering against a vector database."""
+
+from .base import RetrievalQA
--- rwkv_chains/retrieval_qa/prompt.py
+++ rwkv_chains/retrieval_qa/prompt.py
@@ -4,8 +4,9 @@ from ..task_template import alpaca_styled_prompt
 prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
+_______________________
 
-Question: {question}
+According to the context above, answer the question below: {question}
 """
 PROMPT = PromptTemplate(
     template=alpaca_styled_prompt(instruction=prompt_template), input_variables=["context", "question"]
